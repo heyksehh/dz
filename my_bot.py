6226fb88dca91ef7ff92e837f68205372c706b83
heyksehh <ksushonok_5@mail.ru>
2017-05-14 11:39:07 +0300

    Add files via upload

---
+++ my_bot.py
@@ -0,0 +1,45 @@
+﻿import flask
+import telebot
+import conf
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(conf.WEBHOOK_HOST, conf.WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(conf.TOKEN)
+
+bot = telebot.TeleBot(conf.TOKEN, threaded=False) 
+bot.remove_webhook()
+
+bot.set_webhook(url=WEBHOOK_URL_BASE+WEBHOOK_URL_PATH)
+
+app = flask.Flask(__name__)
+
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Здравствуйте! Это бот, который считает количество слов в вашем сообщении.")
+    
+@bot.message_handler(func=lambda m: True)
+
+def send_num(message):
+    num = message.text.split()
+    i = 0
+    for n in num:
+        i += 1
+    bot.send_message(message.chat.id, 'В вашем сообщении {} слов.'.format(i))
+
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
+    return 'ok'
+
+@app.route(WEBHOOK_URL_PATH, methods=['POST'])
+def webhook():
+    if flask.request.headers.get('content-type') == 'application/json':
+        json_string = flask.request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        flask.abort(403)
+        
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
